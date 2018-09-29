f58fabe51ab14bd10a475062900d6fc4cfc20638
Oscar <oskralvarez814@gmail.com>
2018-09-28 22:11:40 -0600

    SignUp v1.1
    Validacion de los campos: email, password.

--- js/controlador-sign-up.js
+++ js/controlador-sign-up.js
@@ -5,15 +5,80 @@ $(document).ready(function(){
     });
 
     //Validacion campos
+    var emailValido;
+    var passValido;
     $("#user-email").keyup(function(){
         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if(regex.test($("#user-email").val())){
-            console.log("valido");
+            $("#valid-email").attr("style", "display:inline");
+            $("#invalid-email").attr("style", "display:none");
+            ////////////
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:none");
+            emailValido = true;
         }else{
-            console.log("invalido");
+            $("#invalid-email").attr("style", "display:inline");
+            $("#valid-email").attr("style", "display:none");
+            emailValido = false;
         }
     });
-    $("btn-cc2").click(function(){
 
+    $("#user-password").keyup(function(){
+       
+        if($("#user-password").val().length>=4){
+            $("#valid-password").attr("style", "display:inline");
+            $("#invalid-password").attr("style", "display:none");
+            ///////////
+            $("#password-vacio").attr("style", "display:none");
+            passValido = true;
+        }else{
+            $("#invalid-password").attr("style", "display:inline");
+            $("#valid-password").attr("style", "display:none");
+            passValido = false;
+        }
+    });
+    $("#btn-cc2").click(function(){
+        //CORREO, CONTRASENIA VALIDO
+        if(emailValido == true && passValido == true){
+            console.log("correo y contra valido"); //Capturar informacion y seguir con siguiente paso
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:none");
+            $("#password-vacio").attr("style", "display:none");
+            //CORREO, CONTRASENIA INVALIDA
+        }else if(emailValido == false && passValido == false){
+            console.log("correo y contra ivalido"); 
+            $("#texto-invalid-email").attr("style", "display:inline");
+            $("#password-vacio").attr("style", "display:inline");
+            //CORREO INVALIDO, CONTRASENIA VALIDA
+        }else if(emailValido == false && passValido == true){
+            console.log("correo in contra va");
+            $("#texto-invalid-email").attr("style", "display:inline");
+            $("#email-vacio").attr("style", "display:none");
+            $("#password-vacio").attr("style", "display:none");
+            //CORREO VALIDO, CONTRASENIA INVALIDA
+        }else if(passValido == false && emailValido == true){
+            console.log("correo co contra in");
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:none");
+            $("#password-vacio").attr("style", "display:inline");
+            //CAMPO CORREO, CONTRASENIA VACIO
+        }else if($("#user-email").val() == "" && $("#user-password").val() == ""){
+            console.log("falta correo y contra");
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:inline");
+            $("#password-vacio").attr("style", "display:inline");
+            //CAMPO CORREO VACIO
+        }else if($("#user-email").val() == "" && $("#user-password").val() != ""){
+            console.log("falta correo");
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:inline");
+            $("#password-vacio").attr("style", "display:none");
+            //CAMPO CONTRASENIA VACIO
+        }else if($("#user-password").val() == "" && $("#user-email").val() != ""){
+            console.log("falta contra");
+            $("#texto-invalid-email").attr("style", "display:none");
+            $("#email-vacio").attr("style", "display:none");
+            $("#password-vacio").attr("style", "display:inline");
+        }
     });
 });
