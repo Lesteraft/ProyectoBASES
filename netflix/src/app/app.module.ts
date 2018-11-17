d9571e7906125fe45df1529b520cee0be3a8179a
Oscar <oskralvarez814@gmail.com>
2018-11-17 09:58:44 -0600

    Librerias

--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarSingInComponent } from './components/sing_in/navbar/navbar.sing_in.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarSingInComponent,
   ],
   imports: [
     BrowserModule,
---
+++ netflix/src/app/components/sing_in/navbar/navbar.sing_in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-sing-in',
+  templateUrl: './navbar.sing_in.component.html',
+  styleUrls: ['./navbar.sing_in.component.css']
+})
+export class NavbarSingInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
