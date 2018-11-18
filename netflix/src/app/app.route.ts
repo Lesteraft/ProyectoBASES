c0d739a9dcb911c6c53b9de46823871cc0eaa91c
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-11-17 19:54:42 -0600

    A1.5

--- netflix/src/app/app.route.ts
+++ netflix/src/app/app.route.ts
@@ -5,15 +5,21 @@ import { RegistroSinginComponent } from './components/sing_in/body/registro-sing
 import { RegformSinginComponent } from './components/sing_in/body/regform-singin/regform-singin.component';
 import { MetodopagoSinginComponent } from './components/sing_in/body/metodopago-singin/metodopago-singin.component';
 import { InfopagoSinginComponent } from './components/sing_in/body/infopago-singin/infopago-singin.component';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilesComponent } from './components/perfiles/perfiles-usuarios/perfiles.component';
+
 
 const APP_ROUTES: Routes = [
     { path: 'inicio', component: InicioSinginComponent },
     { path: 'planes', component: PlanesSinginComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroSinginComponent },
     { path: 'regform', component: RegformSinginComponent },
     { path: 'metodopago', component: MetodopagoSinginComponent },
     { path: 'infopago', component: InfopagoSinginComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+    { path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+    { path: 'perfiles', component: PerfilesComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'login' }
 
 ];
 
