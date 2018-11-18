9eac56b3194dd23aad0fe7f01eed707a91ea1871
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-11-17 19:32:13 -0600

    A0.5

--- netflix/src/app/app.route.ts
+++ netflix/src/app/app.route.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { InicioSinginComponent } from './components/sing_in/body/inicio-singin/inicio-singin.component';
 import { PlanesSinginComponent } from './components/sing_in/body/planes-singin/planes-singin.component';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilesComponent } from './components/perfiles/perfiles-usuarios/perfiles.component';
+
 
 const APP_ROUTES: Routes = [
     { path: 'inicio', component: InicioSinginComponent },
     { path: 'planes', component: PlanesSinginComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+    { path: 'login', component: LoginComponent },
+    { path: 'perfiles', component: PerfilesComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'login' }
 
 ];
 
