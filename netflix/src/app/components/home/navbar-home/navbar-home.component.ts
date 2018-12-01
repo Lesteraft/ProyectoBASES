1cafe7fb4ea63b4becf8fde54de86e3e7905b3dd
Oscar <oskralvarez814@gmail.com>
2018-12-01 10:31:58 -0600

    Search v2.0

--- netflix/src/app/components/home/navbar-home/navbar-home.component.ts
+++ netflix/src/app/components/home/navbar-home/navbar-home.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
 
+
 @Component({
   selector: 'app-navbar-home',
   templateUrl: './navbar-home.component.html',
   styleUrls: ['./navbar-home.component.css']
 })
+
+
 export class NavbarHomeComponent implements OnInit {
 
   i = 0;
@@ -14,15 +17,25 @@ export class NavbarHomeComponent implements OnInit {
   constructor( private _router: Router ) { }
 
   ngOnInit() {
+  let textoBusqueda: any;
+  let activeSearch: Boolean = false;
     $('#icon-search').click(function() {
-        $('#input-search').attr('style', 'display:inline-block');
-        $('#input-search').animate({
-          width: '205px'
-        }, 400, function() {
-        });
-        $('#input-search').attr('style', 'border-left:none');
-        $('.icon').attr('style', 'border: 1px solid #fff; border-right:none; padding:0; height:30px');
-        $('#input-search').focus();
+        if (activeSearch === false) {
+          $('#input-search').attr('style', 'display:inline-block');
+          $('#input-search').animate({
+            width: '205px'
+          }, 400, function() {
+          });
+          $('#input-search').attr('style', 'border-left:none');
+          $('.icon').attr('style', 'border: 1px solid #fff; border-right:none; padding:0; height:30px');
+          $('#input-search').focus();
+          activeSearch = true;
+        } else {
+          textoBusqueda = $('#input-search').val();
+          console.log(textoBusqueda + 'icon');
+
+        }
+
     });
 
     $(window).click(function(e: any) {
@@ -38,9 +51,17 @@ export class NavbarHomeComponent implements OnInit {
             }, 200, function() {
             });
             $('.icon').attr('style', '');
+            activeSearch = false;
             break;
         }
-    })
+    });
+
+    $('#input-search').keypress(function(e: any) {
+        if ( e.which === 13 ) {
+            textoBusqueda = $('#input-search').val();
+            console.log(textoBusqueda);
+        }
+    });
   }
 
   mostrar() {
