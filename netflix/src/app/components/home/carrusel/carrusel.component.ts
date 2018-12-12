735bedf6e04fd72d19cbb8805e804aa8256e692e
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-12-12 02:19:15 -0600

    4.7  reparación de scroll

--- netflix/src/app/components/home/carrusel/carrusel.component.ts
+++ netflix/src/app/components/home/carrusel/carrusel.component.ts
@@ -172,12 +172,12 @@ export class CarruselComponent {
   constructor () {}
 
   moverIzquierda() {
-    console.log('se hizo click izquieda ' + '#' + this.categoria);
-    $('#' + this.categoria).animate({ scrollLeft: '-=1300' }, 1300);
+    console.log('se hizo click izquieda ' + '#' + this.categoria.nombre);
+    $('#' + this.categoria.nombre).animate({ scrollLeft: '-=1300' }, 1300);
   }
 
   moverDerecha() {
-    console.log('se hizo click derecha ' + '#' + this.categoria);
-    $('#' + this.categoria).animate( {scrollLeft: '+=1300' }, 1300);
+    console.log('se hizo click derecha ' + '#' + this.categoria.nombre);
+    $('#' + this.categoria.nombre).animate( {scrollLeft: '+=1300' }, 1300);
  }
 }
